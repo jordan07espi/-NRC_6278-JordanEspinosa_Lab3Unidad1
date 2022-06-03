2d0ad60bb73cb11dbe4f12af15064a560eb6e306
jordan07espi <103512603+jordan07espi@users.noreply.github.com>
2022-06-03 01:33:51 -0500

    Se añade el método agregar_arista

--- [NRC_6278] JordanEspinosa_Lab3Unidad1.py
+++ [NRC_6278] JordanEspinosa_Lab3Unidad1.py
@@ -35,3 +35,33 @@ class Grafo:
             dirigido: boolean
                 Grafo dirigido o No dirgido
         """
+        #Número de nodos
+        self.m_numero_de_nodos = numero_de_nodos
+        #Rango del número de nodos
+        self.m_nodos = range(self.m_numero_de_nodos)
+        # Especifica si es dirigido o no dirigido
+        self.m_dirigido = dirigido
+        # Representación gráfica - Lista de adyacencia
+        # Utilizamos un diccionario para implementar una lista de adyacencia
+        self.m_lista_adyacencia = {node: set() for node in self.m_nodos}#Setea los nodos mediante un ciclo de repetición      
+	
+    # Agrega una arista al grafo
+    def agregar_arista(self, nodo1, nodo2, peso=1):
+        """
+        Agrega una arista al grafo.
+        Pasa al siguiente grafo si el primero no se encuentra dirigido
+        Parámetros:
+        ----------
+        nodo1: int
+            Nodo 1 - Inicio
+        nodo1: int
+            Nodo 2 - Fin
+        peso: int
+            Peso de la arista
+        """
+        #Añade una arista al nodo 1 para la lista del grafo
+        self.m_lista_adyacencia[nodo1].add((nodo2, peso))
+        #Se añáde al otro nodo una arista si grafo resulta no estar dirigido
+        if not self.m_dirigido:
+            #Añade una arista al nodo 2 para la lista del grafo
+            self.m_lista_adyacencia[nodo2].add((nodo1, peso))
